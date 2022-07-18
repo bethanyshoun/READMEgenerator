f49404cde19ef7d19a2bb10eca148037a91136d3
bethanyshoun <bethanyshoun@gmail.com>
2022-07-17 23:09:25 -0400

    began adding functiions and objects to array

--- index.js
+++ index.js
@@ -1,13 +1,51 @@
 // TODO: Include packages needed for this application
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+const inquirer = require('inquirer');
 
 // TODO: Create an array of questions for user input
-const questions = [];
-
+const questions = () => { 
+    return inquirer.prompt([
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of your project?',
+        validate: titleInput => {
+            if (titleInput) {
+              return true;
+            } else {
+              console.log('You need to enter a project title!');
+              return false;
+            }
+          }
+    },
+]);
+}
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
 
 // TODO: Create a function to initialize app
-function init() {}
+const init = readmeData => {
+    console.log(`
+==========================
+Create Professional Readme
+==========================
+`);
+/*
+    //If no array, create one
+    if (!readmeData.projects) {
+        readmeData.projects = [];
+    } 
+    */
+    return inquirer
+        .prompt([""
+
+        ]);
+};
 
 // Function call to initialize app
-init();
+init()
+    .then(questions)
+    .then(readmeData => {
+        return generateMarkdown(readmeData);
+    });
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,3 +1,8 @@
+//Needed?? 
+
+const fs = require('fs');
+
+
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {}
