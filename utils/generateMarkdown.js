f09b0a7ec4714d2ead3c24188723cded27451bbd
bethanyshoun <bethanyshoun@gmail.com>
2022-08-01 14:50:06 -0400

    minor revision in comment

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -43,7 +43,7 @@ function renderLicenseSection(license) {
   
 }
 
-// TODO: Create a function to generate markdown for README
+//Function to generate markdown for README
 function generateMarkdown (data) {
   return `# ${data.title}
 
